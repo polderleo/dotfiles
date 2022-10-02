127969e588fe475b2835347ac914dc3ddbe18464
Niklas Ravnsborg <niklasravnsborg@gmail.com>
2022-10-02 03:41:33 +0200

    add Spotify, Linear, Zoom and Notion to Finicky

--- .finicky.js
+++ .finicky.js
@@ -1,3 +1,6 @@
+// Finicky on GitHub: https://github.com/johnste/finicky
+// Search for integration on GitHub: https://github.com/search?q=filename%3A.finicky.js+Spotify&type=Code
+
 module.exports = {
   defaultBrowser: "Firefox",
   options: {
@@ -8,6 +11,22 @@ module.exports = {
     {
       match: "meet.google.com/*",
       browser: "Chromium"
+    },
+    {
+      match: "open.spotify.com/*",
+      browser: "Spotify",
+    },
+    {
+      match: "linear.app/plan3t/*",
+      browser: "Linear",
+    },
+    {
+      match: "*.zoom.us/j/*",
+      browser: "us.zoom.xos",
+    },
+    {
+      match: /notion\.so\/.*[a-f0-9]{32}/,
+      browser: "Notion"
     }
   ]
 }
