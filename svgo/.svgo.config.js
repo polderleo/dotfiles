45b0c754d27a654e7bf5aaf9ac7903f7b52f6cef
Niklas Ravnsborg <niklasravnsborg@gmail.com>
2022-12-19 16:24:31 +0100

    update svgo config

--- svgo/.svgo.config.js
+++ svgo/.svgo.config.js
@@ -4,7 +4,7 @@ module.exports = {
       name: "preset-default",
       params: {
         overrides: {
-          // convertPathData: false,
+          convertPathData: false,
         },
       },
     },
