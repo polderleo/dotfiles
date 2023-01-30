9f0bdba4cc9aa04128a34ee8d03e3e5a48669959
Niklas Ravnsborg <niklasravnsborg@gmail.com>
2023-01-30 23:25:26 +0100

    add exa aliases

--- shell/.aliases.sh
+++ shell/.aliases.sh
@@ -5,3 +5,8 @@ alias svgo="svgo --config=$HOME/.svgo.config.js"
 
 # kubectl alias
 alias kc="kubectl"
+
+# exa alias
+alias ls='exa --icons --group-directories-first'
+alias ll='exa -l --icons --no-user --group-directories-first  --time-style long-iso'
+alias la='exa -la --icons --no-user --group-directories-first  --time-style long-iso'
