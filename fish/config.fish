c9498b2a5a6fcda120f4fc081efd18b70512bd3f
Niklas Ravnsborg <niklasravnsborg@gmail.com>
2023-02-08 10:13:53 +0100

    fix iTerm doesn't open new tab in same directory

--- fish/config.fish
+++ fish/config.fish
@@ -6,8 +6,8 @@ bass source ~/.env.sh
 # Set aliases
 source ~/.aliases.sh
 
-# Enable iTerm2 shell integration
-source ~/.iterm2_shell_integration.fish
-
 # Init starship
 starship init fish | source
+
+# Enable iTerm2 shell integration
+source ~/.iterm2_shell_integration.fish
