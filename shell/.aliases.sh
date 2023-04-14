3564b21686c71ded00e2473eff2313cb919fef21
Niklas Ravnsborg <niklasravnsborg@gmail.com>
2023-04-14 19:02:37 +0200

    use GNU versions of tar and sed

--- shell/.aliases.sh
+++ shell/.aliases.sh
@@ -11,3 +11,9 @@ alias ls='exa     --icons --classify --group-directories-first'
 alias ll='exa -l  --icons --classify --group-directories-first --header --group --created --modified --octal-permissions --time-style long-iso --git'
 alias la='exa -la --icons --classify --group-directories-first --header --group --created --modified --octal-permissions --time-style long-iso --git'
 alias lt='exa     --icons --classify --group-directories-first --tree --level=2'
+
+# sed alias
+alias sed='gsed'
+
+# tar alias
+alias tar='gtar'
