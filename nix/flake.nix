bdec2fb8d636a620ed9360b201e6ea487d01d558
Niklas Ravnsborg <niklasravnsborg@gmail.com>
2024-05-06 13:27:24 +0200

    install Home Manager for Nix

--- nix/flake.nix
+++ nix/flake.nix
@@ -8,15 +8,28 @@
       url = "github:lnl7/nix-darwin";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs = { self, nixpkgs, darwin }: {
+  outputs = { self, nixpkgs, darwin, home-manager }: {
 
     formatter.aarch64-darwin = nixpkgs.legacyPackages.aarch64-darwin.nixpkgs-fmt;
 
     darwinConfigurations."Niklas-Machbuch" = darwin.lib.darwinSystem {
       system = "aarch64-darwin"; # "x86_64-darwin" if you're using a pre M1 mac
-      modules = [ ./default.nix ];
+      modules = [
+        ./default.nix
+        home-manager.darwinModules.home-manager
+        {
+          home-manager = {
+            useGlobalPkgs = true;
+            users.nik = import ./home.nix;
+          };
+        }
+      ];
     };
 
   };
---
+++ nix/home.nix
@@ -0,0 +1,7 @@
+{ pkgs, config, ... }:
+{
+  home.stateVersion = "23.11";
+
+  # Let Home Manager install and manage itself.
+  programs.home-manager.enable = true;
+}
