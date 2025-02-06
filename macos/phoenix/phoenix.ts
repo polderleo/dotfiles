e9307d665e85dd305ed4f047d21d6efe6faa5115
Niklas Ravnsborg <niklasravnsborg@gmail.com>
2025-02-06 14:18:49 +0100

    setup keyboard shortcuts with Phoenix and Hyperkey

---
+++ macos/phoenix/phoenix.ts
@@ -0,0 +1,23 @@
+// Inspiration: https://github.com/denolfe/dotfiles/tree/main/phoenix
+
+Phoenix.set({
+  daemon: true,
+  openAtLogin: true,
+});
+
+const hyperKey: Phoenix.ModifierKey[] = ["control", "shift", "option", "cmd"];
+
+const bindApp = (key: string, appName: string) => {
+  Key.on(key, hyperKey, () => {
+    App.launch(appName, { focus: true });
+  });
+};
+
+bindApp(";", "Ghostty");
+bindApp("b", "Arc");
+bindApp("e", "Windsurf");
+bindApp("f", "Finder");
+bindApp("c", "Beeper");
+bindApp("a", "Notion Calendar");
+bindApp("m", "Missive");
+bindApp("t", "Microsoft Teams");
