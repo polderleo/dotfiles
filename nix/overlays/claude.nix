b2cb26350aea3993723690b134da7212ebe6e383
Niklas Ravnsborg <niklasravnsborg@gmail.com>
2025-04-14 13:32:31 +0200

    update claude-code overlay

--- nix/overlays/claude.nix
+++ nix/overlays/claude.nix
@@ -1,13 +1,14 @@
+# I maintain this claude-code overlay that uses bun instead of node.
 # To disable the auto updater, run `claude config set --global autoUpdaterStatus disabled`
 
 final: prev: {
   claude-code = final.stdenv.mkDerivation {
     name = "claude";
-    version = "0.2.14";
+    version = "0.2.69";
 
     src = final.fetchurl {
-      url = "https://registry.npmjs.org/@anthropic-ai/claude-code/-/claude-code-0.2.14.tgz";
-      sha256 = "sha256-+q1YgTSQFnldaOPi26FgKrE1mX4uQw73Y9wKD+BCXS4==";
+      url = "https://registry.npmjs.org/@anthropic-ai/claude-code/-/claude-code-0.2.69.tgz";
+      sha256 = "sha256-bQjKhTZzItorMXUsqpPgrHHBBmTtHIpcv/bNJU6koCA=";
     };
 
     buildInputs = with final; [
@@ -21,9 +22,7 @@ final: prev: {
       # Create a wrapper script
       cat > $out/bin/claude <<EOF
         #!/bin/sh
-        # We have to set a system shell here because claude-code doesn't seem to be compatible with fish installed via nix.
-        export SHELL=/bin/zsh
-        exec ${final.bun}/bin/bun $out/cli.mjs "\$@"
+        exec ${final.bun}/bin/bun $out/cli.js "\$@"
       EOF
       chmod +x $out/bin/claude
     '';
